feb47ee11d5ad08916a4e22b46b588165bda9f98
Amaanshaikh54 <shaikhamaan78654@gmail.com>
2024-11-06 15:17:37 +0530

    Updated admin.py

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=['id','name','email','department','role','date_joined']
+
+admin.site.register(Employee,EmployeeAdmin)
