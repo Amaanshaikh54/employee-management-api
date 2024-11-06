757e0720a9a07114abef1020a222da0a50c00389
Amaanshaikh54 <shaikhamaan78654@gmail.com>
2024-11-06 15:16:48 +0530

    Updated models.py

--- employees/models.py
+++ employees/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Employee(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100,null=False,error_messages={'invalid':'Enter name!!'})
+    email = models.EmailField(null=True,unique=True,error_messages={'invalid':'This email is already used Try another email'})
     department = models.CharField(max_length=50, null=True, blank=True)
     role = models.CharField(max_length=50, null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
