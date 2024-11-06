383fbc1c215770d7ba9e979e3c1df62dcbfff884
Amaanshaikh54 <shaikhamaan78654@gmail.com>
2024-11-06 17:45:01 +0530

    Updated..

--- employee-management-api/wsgi.py
+++ employee-management-api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for JobProject project.
+WSGI config for employee-management-api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JobProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employee-management-api.settings')
 
 application = get_wsgi_application()
