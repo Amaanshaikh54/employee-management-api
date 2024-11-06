2b2d17ec3d45550775bc5691234457ff518fcd72
Amaanshaikh54 <shaikhamaan78654@gmail.com>
2024-11-06 16:40:10 +0530

    Updated urls.py

--- EmployeeManagement/urls.py
+++ EmployeeManagement/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for EmployeeManagement project.
+URL configuration for JobProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+# from .views import E
+# from EmployeeCRUD import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('employees.urls')),
-    path('api-token-auth/', obtain_auth_token),
+    path('api/',include('EmployeeCRUD.urls')),
 ]
